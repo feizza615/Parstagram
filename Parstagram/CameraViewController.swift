588bd738edf52d8ec33ef85d1dde18028445918f
Feizza Fazilatun <feizza01@gmail.com>
2021-03-14 14:00:07 -0400

    done

--- Parstagram/CameraViewController.swift
+++ Parstagram/CameraViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import AlamofireImage
+import Parse
 
 class CameraViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
@@ -20,6 +22,22 @@ class CameraViewController: UIViewController, UIImagePickerControllerDelegate, U
     }
     
     @IBAction func onSubmitButton(_ sender: Any) {
+        let post = PFObject(className: "Posts")
+        post["caption"] = commentField.text
+        post["author"] = PFUser.current()
+        let imageData = imageView.image!.pngData()
+        let file = PFFileObject(name: "image.png", data: imageData!)
+        post["image"] = file
+        
+        post.saveInBackground { (success, error) in
+            if success{
+                self.dismiss(animated: true, completion: nil)
+                print("saved!")
+            }else{
+                print("error!")
+            }
+        }
+        
     }
     
     @IBAction func onCameraButton(_ sender: Any) {
@@ -38,6 +56,11 @@ class CameraViewController: UIViewController, UIImagePickerControllerDelegate, U
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         let image = info[.editedImage] as! UIImage
+        let size = CGSize(width: 300, height: 300)
+        let scaledImage = image.af.imageScaled(to: size)
+        imageView.image = scaledImage
+        
+        dismiss(animated: true, completion: nil)
         
     }
     /*
